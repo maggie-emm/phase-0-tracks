225797f3e4d528b2a946352f4599006e84a43ef9
maggie-emm <revmaggie10@gmail.com>
2016-06-01 11:07:03 -0700

    Changed condition stmt formats and gets.chomp answers to y/n from true/false

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,14 +8,14 @@
 #Ask following questions:
 #Name:
 puts "What is your name?"
-name = gets.chomp
+name = gets.chomp.to_s
 
 #age & year_born
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts "What year were you born?"
-year_born = gets.chomp
+year_born = gets.chomp.to_i
 
 #see if age jives with year born
 #get current year
@@ -23,7 +23,7 @@ year_born = gets.chomp
 #puts "Hold a moment while we verify your age."
 current_year = Time.new.year
 	if current_year.to_i - age.to_i == year_born.to_i
-		age = true 
+		age = gets.chomp.to_i 
 	else current_year.to_i - age.to_i != year_born
 		age = false
 	end
@@ -31,29 +31,46 @@ current_year = Time.new.year
 
 #order garlic bread for you from the cafeteria?
 puts "Our cafeteria has the best garlic bread. Should we order some for you? (y/n)"
-	garlic_preference = gets.chomp
-  		if garlic_preference == "y"
+garlic_preference = gets.chomp
+  	if garlic_preference == "y"
   		   garlic_preference = true
-  		else garlic_preference = false
-  		end
+  	else garlic_preference = false
+  	end
 #WORKS
 
 #health insurance?
 puts "Would you like to be enrolled in the company's health insurance?"
-	health_insurance_preference = gets.chomp
-		if health_insurance_preference == "y"
+health_insurance_preference = gets.chomp
+	if health_insurance_preference == "y"
 			health_insurance_preference = true
-		else health_insurance_preference = false
-		end	
+	else health_insurance_preference = false
+	end	
 #WORKS		
 
-#Condition 1 variables
-age_computes = true
-garlic_preference = true
-health_insurance_preference = true
+#Rel 2: Put conditions in place
+#Condition 1 variables: all true = prob not vampire
+	if (age == true) && (garlic_preference == true || health_insurance_preference = true)
+	puts "Probably not a vampire"
+	end
+#YEAH!! WORKS	
 
-(age_computes && garlic_preference || health_insurance_preference)
-puts "Probably not a vampire"
+#Condition 2 variables: all false = prob vampire
+	if (age == false) && (garlic_preference == false || health_insurance_preference = false)
+	puts "Probably a vampire"
+	end
+#YEAH!! WORKS
+
+#Condition 3 variables: all false = prob vampire
+	if (age == false) && (garlic_preference == false) && (health_insurance_preference = false)
+	puts "Almost certainly a vampire"
+	end
+
+
+
+
+
+
+
 
 #condition 2 variables
 age_computes = false
