29439b9204dd3c9370f36ab5a16c3f30fddb8a93
maggie-emm <revmaggie10@gmail.com>
2016-06-01 23:48:34 -0700

    Provide classes to str in method_detective.rb to achieve desired result

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,50 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+"iNvEsTiGaTiOn".swapcase!
+#=> “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+"zom".insert(1, "o")
+"zom".insert(2, "o")
+#=> “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
+"Stop! You’re under arrest!".upcase!
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+a = "the usual "
+a << "suspects"
+
+"the usual " + "suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
+#" suspects".<???>
+a = " suspects"
+a.prepend "the usual"
+
+" suspects".insert(0, "the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chomp("r")
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
+a = "he mystery of the missing first letter"
+a.prepend "T"
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+"122 is ASCII Code = z"
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
