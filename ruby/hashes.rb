fbf2e998cf8fae07c7227e570f804cd57eae02e5
maggie-emm <revmaggie10@gmail.com>
2016-06-02 22:19:41 -0700

    Created hashes.rb and started pseudocode by identifying scope of program

---
+++ ruby/hashes.rb
@@ -0,0 +1,19 @@
+#pseudocode & write program for interior design client form
+#identify all client info that will be needed
+#include str, i && boolean types of data objects
+
+#use symbols for keys unless a str is needed
+
+#Prompt designer/user for all info
+
+#Convert any user input to appropriate data type
+
+#Print hash back out to the screen when the designer has answered
+#   all the questions
+
+#Give the user the opportunity to update a key (no need to loop,
+#   once is fine). If designer says "none", skip it. But if designer
+#   enters "decor_theme" or any new key, program should ask for new
+#   value and update the key. ****See the hint in challenge info***
+
+#Print the latest version of hash & exit program. 
