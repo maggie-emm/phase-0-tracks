7540a6386b544116ee01e31d0d6621979b476cb1
maggie-emm <revmaggie10@gmail.com>
2016-06-06 03:33:29 -0700

    updated alias fil: 5.6

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,32 @@
+#input real name 
+puts "Welcome to Kre8-a-name, trusted by spy's around the globe.\rPlease enter the full name you want disguised. \rIf you are done type 'quit'."
+
+#split & swap first/last name
+real_name = gets.chomp.downcase.split(' ').to_a
+real_name_letters = real_name.shuffle!
+#real_name_letters.map.with_index{ |a, u| a * i }
+#real_name_letters.gsub(/[a]/, "e")
+
+
+index = 0
+spy_name = real_name
+while index <real 
+
+
+
+
+#v = ["a", "e", "i", "o", "u"]
+#real_name_letters.rotate!
+
+
+
+#real_name_letters.map!
+
+#vowels = ["a", "e", "i", "o", "u"]
+#	if vowels.include?(letter)
+#		letter_index = vowel.index(letter)
+#		new_letter_index = letter_index +1
+#		letter = vowels[new_letter-index]
+#end
+#}
+#consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "b"]
