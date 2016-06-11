2b59eab409e7278cc94b8a12d9a18da175a588d5
maggie-emm <revmaggie10@gmail.com>
2016-06-11 07:50:06 -0700

    Calc commit

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -1,12 +1,23 @@
 #Q2: Calculate method for all 4 operations: +, -, *, /
 
+#p 5+4
+#p 9-4
+#p 9/3
+#p 3*3
+
+
+
+
+#def variable names
 def calculate
-	add_integers = 5 + 4
-	
+  add_integers = [a "+" b]
+  sub_integers = [c "-" d]
+  div_integers = [e "/" f]
+  mult_integers = [g "*" h]
 end
 
-p add_integers
 
+#
 
 
 class Calculator
