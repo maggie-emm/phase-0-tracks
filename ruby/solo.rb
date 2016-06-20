a72c4ea45910f62c69057cde68e139ac22cba6a5
maggie-emm <revmaggie10@gmail.com>
2016-06-20 08:23:08 -0700

    Update pseudocode

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,7 +1,7 @@
 #design: class Patterns
-#patterns can either be:
+#patterns have following attributes:
 #  sew
-#  crochet: 
+#  crochet 
 #  knit
 #  bead
 #  craft
@@ -10,14 +10,24 @@
 class Patterns
 
 # 3 attributes w/ 2 data types; values vary; i.e.; age
-# 1 attr & data_type: crochet: hook_size(str: a, b, c, q), est_time(str), num_colors(int)
-# 2 attr & data_type: sew: clothing(boolean); diff_level(str: easy, med, hard)
-# 3 attr & data_type: beads: type(str: necklace, bracelet, earring); colors(str)
+# 1 attr & data_type: 
+	# :crochet
+	# => hook_size(str: d, f, q)
+	# => est_time(str: hours, days)
+	# => num_colors(int)
+# 2 attr & data_type: 
+	# :sew
+	# => clothing(boolean: T/F)
+	# => diff_level(str: easy, med, hard)
+# 3 attr & data_type: 
+	# :beads
+	# => type_design(str: necklace, bracelet, earring)
+	# => base_color(str: silver, gold, bronze)
 # 3 methods w/ 1  takes an argument
-	# arg=boolean if/else
+	# arg=boolean if/else > sew: Is this an item of clothing? (y/n)
 
-#attr that will not change:
+#attributes that will not change:
 	attr_reader 
 
-#attr avail to be set & changed:
+#attributes that are accessable to be set & changed:
 	attr_accessor :crochet, :sew, :beads
