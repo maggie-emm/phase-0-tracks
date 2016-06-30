a7e3b4122d65231fabb9c37b21f6ea0d9802ff1d
maggie-emm <revmaggie10@gmail.com>
2016-06-30 13:41:16 -0700

    Pulling master from github to sync w/local files

---
+++ ruby/github_calculator.rb
@@ -0,0 +1,37 @@
+#Q2: Calculate method for all 4 operations: +, -, *, /
+
+#p 5+4
+#p 9-4
+#p 9/3
+#p 3*3
+
+
+
+
+#def variable names
+def calculate
+  add_integers = [a "+" b]
+  sub_integers = [c "-" d]
+  div_integers = [e "/" f]
+  mult_integers = [g "*" h]
+end
+
+
+#
+
+
+class Calculator
+  def add(x,y)
+    x + y
+  end
+
+  def subtract(x,y)
+    x - y
+  end
+
+  def multiply(x,y)
+    x * y
+  end
+end
+Status API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
---
+++ ruby/github_puppy.rb
@@ -0,0 +1,20 @@
+# create puppy class in pseudocode
+#PUPPY CLASS DESIGN
+# SPECIES ------------------------
+# Puppiulous Socuteofus
+
+# CHARACTERISTICS ----------------
+# Number of legs: 4
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+# Activity level: high energy
+
+
+# BEHAVIOR -----------------------
+# Jump
+# Lick
+# Bite
+# Eat
+# Sleep
