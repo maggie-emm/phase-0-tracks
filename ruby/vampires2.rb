c1d08491c3ed48749c2792a24096cdb4204490b7
medorseymidd <maggie.emm2@gmail.com>
2016-08-28 14:02:22 -0700

    create file vampires2.rb. add pseudocode for Rel1: Data Gathering questions

---
+++ ruby/vampires2.rb
@@ -0,0 +1,10 @@
+#WEREWOLF Inc; new employee interview process
+
+#REL 1: DATA GATHERING STAGE:
+#Ask new employees following questions:
+
+#What is you name? (Str)
+#How old are you? (Int)
+#What year were you born? (Int)
+#Would you like an order of garlic bread from the cafeteria? (y/n)
+#Would you like to enroll in company's health insurance? (y/n)
