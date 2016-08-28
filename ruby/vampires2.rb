f240f87889adaa6ae9f7f7eab4fce01ed3352b7a
medorseymidd <maggie.emm2@gmail.com>
2016-08-28 15:22:39 -0700

    update data gathering code for until loop on y/n questions

--- ruby/vampires2.rb
+++ ruby/vampires2.rb
@@ -12,23 +12,37 @@ puts "How old are you?" #(Int)
 puts "What year were you born?" #(Int)
 	year_born = gets.chomp
 
+
+valid_input = false
+until valid_input
+
 puts "Would you like an order of garlic bread from the cafeteria? (y/n)"
 	galic_preference = gets.chomp.downcase
 
-	valid_input = true
 	if galic_preference == "y"
+		valid_input = true
 		puts "Glad you like garlic. I do too!"
-	else
+	elsif galic_preference == "n"
+		valid_input = true
 		puts "So you're not a garlic fan? hmmm"
+	else
+		puts "It's a simple (y)es or (n)o answer. Try it again"
 	end
+end
+
+valid_input = false
+until valid_input
 
 puts "Would you like to enroll in company's health insurance? (y/n)"
 	insurance_preference = gets.chomp.downcase
 
-	valid_input = true
 	if insurance_preference == "y"
+		valid_input = true
 		puts "Glad to provide you with health insurance."
-	else
+	elsif insurance_preference == "n"
+		valid_input = true
 		puts "No health insurance? hmmm"
+	else
+		puts "It's a simple (y)es or (n)o answer. Try it again"
 	end
-	
+end
