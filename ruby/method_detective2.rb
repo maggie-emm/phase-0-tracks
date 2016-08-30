822423fce6e01fe77188308d4641923a33486d4b
medorseymidd <maggie.emm2@gmail.com>
2016-08-30 11:57:51 -0700

    saved method_detective2.rb practice file

---
+++ ruby/method_detective2.rb
@@ -0,0 +1,52 @@
+# Replace in the "<???>" with the appropriate method (and arguments, if any).
+# Uncomment the calls to catch these methods red-handed.
+
+# When there's more than one suspect who could have
+# committed the crime, add additional calls to prove it.
+"iNvEsTiGaTiOn".swapcase
+"iNvEsTiGaTiOn".swapcase!
+#=> “InVeStIgAtIoN”
+
+"zom".insert(1, "o")
+"zom".insert(2, "o")
+#=> “zoom”
+
+"enhance".center(15)
+# => "    enhance    "
+
+"Stop! You’re under arrest!".upcase
+"Stop! You’re under arrest!".upcase!
+# => "STOP! YOU’RE UNDER ARREST!"
+
+a = "the usual "
+a << "suspects"
+
+"the usual " + "suspects"
+#=> "the usual suspects"
+
+#" suspects".<???>
+a = " suspects"
+a.prepend "the usual"
+
+" suspects".insert(0, "the usual")
+# => "the usual suspects"
+
+"The case of the disappearing last letter".chomp("r")
+"The case of the disappearing last letter".chop
+# => "The case of the disappearing last lette"
+
+# "The mystery of the missing first letter".<???>
+a = "he mystery of the missing first letter"
+a.prepend "T"
+# => "he mystery of the missing first letter"
+
+"Elementary,    my   dear        Watson!".squeeze(" ")
+# => "Elementary, my dear Watson!"
+
+"z".ord
+# => 122 
+# (What is the significance of the number 122 in relation to the character z?)
+"122 is ASCII Code = z"
+
+"How many times does the letter 'a' appear in this string?".count "a"
+# => 4
