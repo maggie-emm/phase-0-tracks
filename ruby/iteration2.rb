8c041b286820e285a1c877368512eb05b1621d3b
medorseymidd <maggie.emm2@gmail.com>
2016-08-30 13:25:16 -0700

    add map! on array

--- ruby/iteration2.rb
+++ ruby/iteration2.rb
@@ -9,8 +9,29 @@
 # dogs << "trig", "pugs", "stella", "chauncey"
 # p dogs
 
-dogs = ["trig", "pugs", "stella", "chauncey"]
-puts "original data:"
-p dogs
+# #array.each
+# dogs = ["trig", "pugs", "stella", "chauncey"]
+# puts "original data:"
+# p dogs
 
-dogs.each { |name| puts "some fav dog names are: #{name}" }
+# dogs.each { |name| puts "some fav dog names are: #{name}" }
+
+#hash.each
+# dogs = {"trig" => "chihuahua", "pugs" => "pug", "chauncey" => "husky"}
+# puts "original data:"
+# p dogs
+
+# dogs.each { |name, breed| puts "#{name} is a #{breed}" }
+
+#array.map
+fruit = ["apple", "orange", "banana"]
+puts "Original data:"
+p fruit
+
+fruit.map! do |type|
+	puts type
+	"veggie's rule"
+end
+
+puts "After .map! call: "
+p fruit
