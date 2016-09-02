f0b0a2ae6a6c436a4741c883b51aca4580d4da06
medorseymidd <maggie.emm2@gmail.com>
2016-09-02 13:25:36 -0700

    create problem_solving_method.rb from MP asyaasha to continue to work on it

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,43 @@
+# define a method
+# check an array for a given number
+# go through each of the numbers in an array - use .lenght
+# use loop while the whole lenght would checked for a number
+#if number is there we need to return the index
+# if it's not there retur nill
+
+
+# def search(array, number)
+#     array = []
+# 	i = 0
+# 	while i != array.length do
+
+#     if array.include? () == true
+#     return number.index
+#     else p nil	
+# end
+
+    
+#  end
+# 		i+=1
+# end
+
+
+
+# array = [66, 89, 23, 1]
+# p search(array, 2)
+
+
+#we start with [0,1]
+# sum of last two numbers
+# push them into the array
+# return a new array
+# push them inside until the lenght of the array equals the desired length 
+#
+
+def fib()
+	array = [0,1]
+	sum = array.index(0) + array.index(1)
+	return sum
+end
+
+print fib()
