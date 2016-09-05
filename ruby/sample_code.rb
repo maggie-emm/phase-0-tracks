4561e8e5f4d6b3eea4cfd658347ba629f91e2019
medorseymidd <maggie.emm2@gmail.com>
2016-09-05 11:06:09 -0700

    create new file for sample code to test & refine

---
+++ ruby/sample_code.rb
@@ -0,0 +1,17 @@
+#code from slack to investigate as to its value
+
+client_info = []
+
+puts 'What is your name?"
+name = gets.chomp 
+client_info.push(name)
+
+
+
+client_info = {} 
+
+puts "What is your first name?"
+name = gets.chomp 
+client_info[:first] = fname 
+
+p client_info
