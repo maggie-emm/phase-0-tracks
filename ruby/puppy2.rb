ebeb144def089d02cb15a022bd900837e98414da
medorseymidd <maggie.emm2@gmail.com>
2016-09-06 08:54:39 -0700

    create puppy2.rb & write pseudocode for Class

---
+++ ruby/puppy2.rb
@@ -0,0 +1,17 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Fur: soft, short
+# Size: small
+# Teeth: sharp, pointed
+# Name: varies
+# Color: varies
+# Bark: high-pitched
+
+# BEHAVIOR -----------------------
+# Jump
+# Bite
+# Sleep
+# Eat
+# Play
