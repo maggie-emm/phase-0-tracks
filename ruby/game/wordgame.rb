508498e71f56b08b089cb619fc6e2cd61f9f08c8
medorseymidd <maggie.emm2@gmail.com>
2016-09-11 16:19:03 -0700

    update psdo.code to reflect Scope, Specs, Business Logic, User Interface and identify methods

--- ruby/game/wordgame.rb
+++ ruby/game/wordgame.rb
@@ -1,28 +1,45 @@
-# class WordGame # user1 writes a word & user2 tries to guess it.
-# user2 gets a "YEAH" message if guessed correctly & "!YEAH" if incorrect
-	
+# SCOPE OF PROJECT: 
+# -class WordGame 
+# - user1 writes a word & 
+# - user2 tries to guess it.
+# - user2 gets a "YEAH" message if guessed correctly & 
+#                "!YEAH" if incorrect
+# SPECS:
 # 	1) 1st user enters a word
-# 		A) ask user1 to write a word
-# 		  I) length of word is determined
-# 		B) based on # letters in word, _ is returned for each letter to 	signify # letters
+# 		A) ask user1 to write a word......................USER INTERFACE
+# 		  I) length of word is determined.................BUS LOGIC: METHOD .length.to_i
+# 		B) based on # letters in word, _ is returned for each letter to signify 
+#  					# letters.............................BUS LOGIC: METHOD = .split(' _')
 # 	2) 2nd user attempts to guess the word.
-# 		A) ask user2 to guess word or guess by letter
-# 		  I)  user2 gets as many guesses as length of the word
-# 		  II) if letter is repeated then computer places letter on "_" 	
-# 		  	  for each instance of letter in the word(???repeat guesses do not count against the user(can have as many guesses they ???..contradicts message above re: limited # of guesses)
+# 		A) ask user2 to guess word or guess by letter..USER INTERFACE
+# 		  I)  user2 has no restriction on num guesses.....BUS LOGIC: METHOD until [].length = 
+#   																		 magic word.length
+# 		  II) if letter is repeated then computer places letter on each "_" 
+#  												..........BUS LOGIC: METHOD if/else	
 # 		  III) player receives continual feedback on the current state 
 # 		  	   of the word; i.e., 
-# 		  	  a) for ea letter guessed correctly, the letter is returned 	to it's correct place(s) in the word
-# 		  	  b) for each letter guessed incorrectly, the letter is 
-# 		  	  	  placed on a "Incorrect Guess" space so user can keep track...(???)
+# 		  	  a) for ea letter guessed correctly, the letter is returned to it's correct 
+#  					place(s) in the word........BUS LOGIC: if magic word.letter.include?(true)
+#                                                         p @ index
+# 		  	  b) for each letter guessed incorrectly, the letter is placed in a 
+# 		  	  	  "Incorrect Guess" space so user can keep track...(???)....MVP..
+#   					...this not in specs.nice to add.MVP so not to be included @this time
 # 			  c) Example: if the secret word is "unicorn", the user will 
 # 			  	  start out seeing something like "_ _ _ _ _ _ _", which would beecome "_ _ _ c _ _ _" after the user enters a guess of "c".
-# 			  d) user gets a congratulatory message if they win
-# 			  e) user gets a taunting message if they lose
+# 			  d) user gets a congratulatory message if they win.....BUS LOGIC METHOD 
+#  											if guessed word = magic word puts "Yeah!"
+# 			  e) user gets a taunting message if they lose..........BUS LOGIC METHOD
+#  											if guessed word != magic word puts "!Yeah"
+# BUSINESS LOGIC:
 
 class Wordgame
 
 	def initialize
 	end
 
-	def game_word
+	def magic_word(word)
+
+	end
+end
+
+# USER INTERFACE:
