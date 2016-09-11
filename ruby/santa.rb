5861d8f82c0ecd993e83574be5875834ece68bb5
medorseymidd <maggie.emm2@gmail.com>
2016-09-10 23:09:30 -0700

    random age and start user interface on santa.rb file

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,25 +1,6 @@
-############## NEW STUFF ###########################
-
-# Define Santa Class:
 class Santa
-# Add code below class declaration to check can intialize class & call methods
-
-
-# santa.speak("Ho, ho, ho! Haaaappy holidays!")
-# santa.eat_milk_and_cookies("gingerbread")
-# Create 3 Instance Methods:
-#  - speak: print "Ho, ho, ho! Haaaappy holidays!"
-#  - eat_milk_and_cookies: takes cookie type as a paramenter(gingerbread) and
-#        prints: "That was a good <type of cookie here>!" 
-#  - initialize: prints: "Initializing Santa instance ...".
-
-# REL 1> Give Santa Attributes
-#   - gender & ethnicity on initialization
-#   - reindeer_ranking, an array of reindeer from most preferred to least preferred	
-#       - not passed in initialization: default value: ["Rudolph", "Dasher", "Dancer", 
-#                        "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-#   - age: not passed in initalization: default: 0
-
+	attr_reader :age, :ethnicity, :name
+	attr_accessor :gender
 
 	def initialize(name, gender, ethnicity)
 		puts "Initializing Santa instance ..."
@@ -38,64 +19,30 @@ class Santa
 		puts "That was a good #{cookie_type} cookie!" 
 	end
 
-#setter method
-	def gender
-		@gender
-	end
-#end setter method
-#getter methods
-	def age
-		@age
-	end
-
-	def ethnicity
-		@ethnicity
-	end
-
-	def name
-		@name
-	end
-#end getter methods
 	def celebrate_birthday(age)
 		@age +=1
 	end
 
 	def get_mad_at(reindeer_name)
-		reindeer_name << reindeer_ranking.last
+	puts "Santa's unhappy with #{reindeer_name}"
+		@reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer_name))
+		@reindeer_ranking << reindeer_name
+	end	
 
-	def about
-		puts "Name: #{@name}"
-		puts "Age: #{@age}"
-		puts "Gender: #{@gender}"
-		puts "Ethnicity: #{@ethnicity}"
-		puts "Reindeer assignment: #{@reindeer_ranking}"
-	end
+	# def about
+	# 	puts "Santa Assignment: "
+	# 	puts "Name: #{@name}"
+	# 	puts "Age: #{@age}"
+	# 	puts "Gender: #{@gender}"
+	# 	puts "Ethnicity: #{@ethnicity}"
+	# 	puts "Reindeer assignment: #{@reindeer_ranking}"
+	# end
 end
 
-
-##DRIVER CODE 2:
-santas = []
-names = ["Jack", "Jill", "Frank", "Nick", "Bob", "Billie", "Sunshine"]
-gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-ethnicity = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-
-puts "Iterating through Available Santa list..."
-
-names.each do |name|
-	santa_gender = gender.sample
-	santa_ethnicity = ethnicity.sample
-	puts "Available Santa details: #{name}, #{santa_gender}, #{santa_ethnicity}"
-	santas << Santa.new(name, santa_gender, santa_ethnicity)
-	puts "There are now #{santas.length} santas on the Available Santa list."
-	puts "======"
-	end
-end
-
-#DRIVER CODE 1:
-santa = Santa.new("Jack", "Bois", "First Peoples")
-santa.about
+santa = Santa.new("Jack", "agender", "black")
+puts "Santa's helper: #{santa.name}, #{santa.age=45}, #{santa.gender}, #{santa.ethnicity}"
+puts "Reindeer Assignment: #{@reindeer_ranking}"
+puts "Age correction: #{santa.celebrate_birthday}"
+puts "Reindeer Reassignment: #{santa.get_mad_at(Prancer)}"
+santa.eat_milk_and_cookies
 santa.speak
-santa.eat_milk_and_cookies("gingerbread")
-p @reindeer_ranking
-santa.age(105)
-santa.about
