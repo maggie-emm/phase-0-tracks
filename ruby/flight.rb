090a7f2190646eefd522c0c5f01a7d23a7a45772
medorseymidd <maggie.emm2@gmail.com>
2016-09-11 03:39:11 -0700

    copy/paste Rel 2 example

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,5 +1,22 @@
-class Bird
+module Flight
+	
+	def take_off (altitude)
+		puts "Taking off and ascending until reaching #{altitude}..."
+	end
 end
+	
+	class Bird
+		include Flight
+	end
 
-class Plane
-end
+	class Plane
+		include Flight
+	end
+
+
+
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
