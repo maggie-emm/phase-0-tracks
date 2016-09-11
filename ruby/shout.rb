2d57db5851f2fdf9f8a860c50b9fc7ecfd17c566
medorseymidd <maggie.emm2@gmail.com>
2016-09-11 03:09:39 -0700

    add class yelling_angrily & yell_happily. add print code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,11 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+  	words = "!!!" + ":("
+  end
+
+  def self.yelling_happily(sounds)
+  	sounds = "YAAAAAY" + "!$$!"
+  end
 end
+
+puts Shout.singleton_methods
