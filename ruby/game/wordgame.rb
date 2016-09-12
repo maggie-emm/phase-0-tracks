2c22d5c65f84eebfc9ab3e108d0957a6e9efba80
medorseymidd <maggie.emm2@gmail.com>
2016-09-11 22:35:30 -0700

    add rest of methods. compare magic_word array with letter_guess array(disgard if != on wordgame.rb file

--- ruby/game/wordgame.rb
+++ ruby/game/wordgame.rb
@@ -7,7 +7,7 @@
 # SPECS:
 # 	1) 1st user enters a word
 # 		A) ask user1 to write a word......................USER INTERFACE: puts & gets.chomp
-# 		  I) what to do with that word: determine ........BUS LOGIC: METHOD steps>ARR?
+# 		  I) what to do with that word: determine ........BUS LOGIC: METHOD steps>STR? ARR?
 #  						- length:  				.length.to_i
 #   					- split by ea char		.each_char
 #   														WHAT IF A HASH???
@@ -20,8 +20,8 @@
 #  																
 # 	2) 2nd user attempts to guess the word.
 # 		A) ask user2 to guess word or guess by letter..USER INTERFACE
-# 		  I)  user2 has no restriction on num guesses.....BUS LOGIC: METHOD until [].length = 
-#   																		 magic word.length
+# 		  I)  user2 has no restriction on num guesses.....BUS LOGIC: METHOD while letter_guess != 
+#   																		 magic word, keep trypng
 # 		  II) if letter is repeated then computer places letter on each "_" 
 #  												..........BUS LOGIC: METHOD if/else	
 # 		  III) player receives continual feedback on the current state 
@@ -41,28 +41,69 @@
 # BUSINESS LOGIC:
 
 class Wordgame
-	attr_reader :word
+	attr_accessor :word
+	attr_accessor :letter_guess
 
-
-	def initialize
-		@word = ('')
+	def initialize(word)
+		@word = word
+		@length = length
+		@letter_guess = letter_guess
 	end
 
 	def magic_word(word)
-		@word.split('')
+		@word = Array.new(word.chars)
+		@word
 	end
 
+	def length(int)
+		@length = word.length
+		puts "This Magic Word has #{game.length} letters!"
+		@length
+	end
+
+	def letter_guess(letter)
+		letter_guess << []
+		letter_guess.keep_if { |l| l =- /[Array.new]/ }
+		if @word.include = true
+			puts "WOW! Are you a mind reader?"
+		else 
+			puts "Naah.. at least you're in the same alphabet :( Try again..."
+		end
+	end
+
+	def about
+		puts "Magic_Word: #{@word}"
+		puts "Magic_Word length: #{@length}"
+		puts "Letter Guess: "
+	end
 end
 
 # USER INTERFACE:
 puts "Welcome to the MAGIC W O R D game!"
-game = Wordgame.new
+puts "And who is the player that will be starting our game?"
+name1 = gets.chomp
 
-puts "Please enter a  M A G I C  W O R D: "
-	word = gets.chomp
-p game.magic_word(@word)
-# p @word
+puts "And who will #{name1} be playing against today?"
+name2 - gets.chomp
 
+puts "AMAZING! OK, #{name1} and #{name2}, on your marks, get set, GO!"
+puts "#{name1}, please enter a  M A G I C  W O R D: "
+word = gets.chomp
+game = Wordgame.new(@word)
 
+puts " _ " * game.length
+puts "OK, #{name2}, you're up! What is your first letter guess?"
+letter_guess = gets.chomp
+
+while game.letter_guess != game.magic_word do
+	puts "Type a letter to guess!"
+	letter = gets.chomp
+	game.letter_location(letter)
+	game.letter_guess(letter)
+end
 
 #### TEST CODE:
+game.magic_word
+game.length
+game.letter_guess
+game.about
