f1059c3e79418cc3077e2b3e3df7c524bed652a4
medorseymidd <maggie.emm2@gmail.com>
2016-09-11 17:56:20 -0700

    add magic_word method & start UI with Wordgame.new & request user for word

--- ruby/game/wordgame.rb
+++ ruby/game/wordgame.rb
@@ -9,7 +9,8 @@
 # 		A) ask user1 to write a word......................USER INTERFACE
 # 		  I) length of word is determined.................BUS LOGIC: METHOD .length.to_i
 # 		B) based on # letters in word, _ is returned for each letter to signify 
-#  					# letters.............................BUS LOGIC: METHOD = .split(' _')
+#  					# letters.............................BUS LOGIC: METHOD str = .split(' ')
+#  																
 # 	2) 2nd user attempts to guess the word.
 # 		A) ask user2 to guess word or guess by letter..USER INTERFACE
 # 		  I)  user2 has no restriction on num guesses.....BUS LOGIC: METHOD until [].length = 
@@ -26,20 +27,35 @@
 #   					...this not in specs.nice to add.MVP so not to be included @this time
 # 			  c) Example: if the secret word is "unicorn", the user will 
 # 			  	  start out seeing something like "_ _ _ _ _ _ _", which would beecome "_ _ _ c _ _ _" after the user enters a guess of "c".
-# 			  d) user gets a congratulatory message if they win.....BUS LOGIC METHOD 
+# 			  d) user gets a congratulatory message if they win.....
 #  											if guessed word = magic word puts "Yeah!"
 # 			  e) user gets a taunting message if they lose..........BUS LOGIC METHOD
 #  											if guessed word != magic word puts "!Yeah"
 # BUSINESS LOGIC:
 
 class Wordgame
+	attr_reader :word
+
 
 	def initialize
+		@word = ('')
 	end
 
 	def magic_word(word)
-
+		@word.split('')
 	end
+
 end
 
 # USER INTERFACE:
+puts "Welcome to the MAGIC W O R D game!"
+game = Wordgame.new
+
+puts "Please enter a  M A G I C  W O R D: "
+	word = gets.chomp
+p game.magic_word(@word)
+# p @word
+
+
+
+#### TEST CODE:
