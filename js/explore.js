6f6801d9a0b4285436c97a94175f9bb326f6a240
medorseymidd <maggie.emm2@gmail.com>
2016-09-13 11:18:37 -0700

    explore.js file: do the thing #2: psuedocode to reverse a string; i.e., hello = olleh

---
+++ js/explore.js
@@ -0,0 +1,23 @@
+// In comments, pseudocode a function that takes
+// a string as a parameter and reverses the string. 
+// reverse("hello"), for example, should return "olleh". 
+// This isn't a task specific to JavaScript -- your 
+// pseudocode should be in plain English and use phrases 
+// like "for each" instead of JavaScript-specific terms. 
+// Remember that you can add strings in JavaScript, 
+// so '' + 'a' would result in the string 'a'. 
+// This is the most valuable skill in programming: 
+// being able to think through a problem logically.
+
+// declare a variable string called "Hello!"
+
+// identify the length of the string
+
+// loop thru string to break out each letter in the string 
+
+// identify the order of the letters; i.e., H=0, e=1, etc
+
+// reverse the order of the letters; start with length-1
+
+// print the resulting new string
+
