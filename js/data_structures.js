0badbe58c7ce836885df1d81cdf8a27a155a65ad
medorseymidd <maggie.emm2@gmail.com>
2016-09-13 07:26:55 -0700

    update for Rel0. build 2 arrays color & horses. added 1 to each

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+// declaring an array in JavaScript is similar to Ruby
+var colors = ["Indigo", "Hot Pink", "Mousey Brown", "Grassy Green"];
+var horses = ["Trigger", "Larry", "Far Out", "Missy Myers"];
+
+colors.push("Plush Purple");
+horses.push("Ginger");
+
+console.log(colors);
+console.log(horses);
