b149489609106eb729eb35c227f8b13ab3f542c1
medorseymidd <maggie.emm2@gmail.com>
2016-09-18 16:09:56 -0700

    create algos.js file.add rel0 pcode instructions

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// Rel0: Find Longest Phrase
+// function to take a str array
+// returns longest word or phrase in array
+// sample=["long phrase", "longest phrase", "longer phrase"]
+// expect .to eq "longest phrase"
+// NOTE: some.string.length will provide expect
+// add driver code
