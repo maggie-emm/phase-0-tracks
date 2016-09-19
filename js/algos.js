f6c82184b2d239a6f9e434bd98ddcd0d57bf0072
medorseymidd <maggie.emm2@gmail.com>
2016-09-18 20:53:43 -0700

    finish rel0 on algos.js file. DEBUG: not pushing var x,y,z into phrase_length arr

--- js/algos.js
+++ js/algos.js
@@ -3,25 +3,40 @@
 var phrases = ["Panthers", "Panthers Rock", "Best QB coach in the league"];
 // spec: returns longest word or phrase in array
 // get length of arr
-phrases.length; 
+phrases.length;
 // # items in var phrases
+phrase_length = [];
+
 var item1 = phrases[0];
+var x = item1.length;
+phrase_length.push[x];
 var item2 = phrases[1];
+var y = item2.length;
+phrase_length.push[y];
 var item3 = phrases[2];
+var z = item3.length;
+phrase_length.push[z];
 // expect longest phrase.to eq "Best QB coach..."
-// var longest_phrase = () {
-	
+// to identify longest, push length integers into arr
+//    phrase_length = [] then id highest digit
+// function compare(item1, item2, item3) {
+// 	// 
+// 	console.log("The longest phrase is: ");
 // }
+
 
 // add driver code
 console.log(phrases);
 console.log(phrases.length);
 console.log(item1); 
 console.log(item1.length);
+console.log(phrase_length[0]);
 console.log(item2); 
 console.log(item2.length);
+console.log(phrase_length[1]);
 console.log(item3); 
 console.log(item3.length);
+console.log(phrase_length[2]);
 
 
 // Rel1: Find Key-Value Match
