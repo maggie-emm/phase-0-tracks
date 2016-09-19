e9fafb4c154bf88050c8c84dceca152dbdae129e
medorseymidd <maggie.emm2@gmail.com>
2016-09-18 18:24:46 -0700

    complete pscode from release directions

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,24 @@
 // Rel0: Find Longest Phrase
-// function to take a str array
+// write a function to take a str array
 // returns longest word or phrase in array
 // sample=["long phrase", "longest phrase", "longer phrase"]
 // expect .to eq "longest phrase"
 // NOTE: some.string.length will provide expect
 // add driver code
+
+// Rel1: Find Key-Value Match
+// write a function that checks 2 object to see
+//   if at least 1 key-value pair matches both objects
+// allow additions to file at top and test at bottom
+// expect when function called to check match .to eq= true
+// i.e.,{name: "Al", age: 54} || {name: "Tip", age:54}=>true
+// add driver code
+
+// Rel2: Generate Random Test Data
+// write function takes a num as the length of an array
+//   of a string with that number of elements
+// expect arr of 3 elements of 3 randomly generated words
+//    specs: random word range (1..11) letters
+// NOTE: involves generating random number
+// add driver code: 10 times: generate arr, print arr, 
+//    feed array to Longest Word function, prints result
